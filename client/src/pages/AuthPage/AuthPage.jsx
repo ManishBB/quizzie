ea9bd28d6d26c8fca80aab6c9e1b46752dcfad09
Manish <mbb7224@gmail.com>
2024-01-22 00:46:16 +0530

    Added Quiz Analysis page

---
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './authpage.module.css'
+
+function AuthPage() {
+  return (
+    <div className={styles.authPage}>
+        <div className={styles.container}>
+            Auth
+        </div>
+    </div>
+  )
+}
+
+export default AuthPage
