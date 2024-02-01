3c685c951e893cae74432be1b7360f33b6639214
Manish <mbb7224@gmail.com>
2024-02-01 15:06:00 +0530

    Updated backend server deployment from render to cyclic

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const conf = {
     serverUrl: String(import.meta.env.SERVER_URL),
-    baseUrl: "https://quizzie-wjkp.onrender.com/api/v1",
+    baseUrl: "https://real-long-johns-elk.cyclic.app/api/v1",
     // baseUrl: "https://peaceful-cranachan-69f7c9.netlify.app/api/v1",
     emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
 };
